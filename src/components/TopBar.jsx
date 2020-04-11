73b95a9ac29cf1e8ecf7da627fdfd1c3413caad7
manish465 <ms5978307@gmail.com>
2020-04-10 17:20:07 -0700

    added a login button

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,11 +1,24 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, Button, Grid } from "@material-ui/core";
+import { Person as PersonIcon } from "@material-ui/icons";
 
 const TopBar = () => {
     return (
         <AppBar>
             <Toolbar>
-                <Typography>SMARKET</Typography>
+                <Grid container justify="space-between">
+                    <Grid xl={6}>
+                        <Typography>SMARKET</Typography>
+                    </Grid>
+                    <Grid xl={6}>
+                        <Typography>
+                            <Button color="inherit" variant="outlined">
+                                <PersonIcon />
+                                Login
+                            </Button>
+                        </Typography>
+                    </Grid>
+                </Grid>
             </Toolbar>
         </AppBar>
     );
